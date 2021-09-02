5c89cc2d398889d85d4c192d09ca3fd6325e4b2b
ZhangHQ <tomcath@foxmail.com>
2021-09-02 17:08:25 +0800

    重写异常返回

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,12 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Routing\Router;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -37,5 +42,57 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * 重写异常
+     * @Author ZhangHQ
+     * @param \Illuminate\Http\Request $request
+     * @param Throwable $e
+     * @return \Illuminate\Http\JsonResponse|\Symfony\Component\HttpFoundation\Response
+     * @throws \ReflectionException
+     */
+    public function render($request, Throwable $e)
+    {
+
+        if (method_exists($e, 'render') && $response = $e->render($request)) {
+
+            return Router::toResponse($request, $response);
+        } elseif ($e instanceof Responsable) {
+
+            return $e->toResponse($request);
+        }
+
+        $e = $this->prepareException($this->mapException($e));
+
+        foreach ($this->renderCallbacks as $renderCallback) {
+            if (is_a($e, $this->firstClosureParameterType($renderCallback))) {
+                $response = $renderCallback($e, $request);
+
+                if (! is_null($response)) {
+                    return $response;
+                }
+            }
+        }
+
+        if ($e instanceof HttpResponseException) {
+
+            return $e->getResponse();
+        } elseif ($e instanceof AuthenticationException) {
+
+            return response([
+                'code' => 403,
+                'messages' => '无权限访问',
+                'data' => []
+            ], 403);
+        } elseif ($e instanceof ValidationException) {
+
+            return $this->convertValidationExceptionToResponse($e, $request);
+        }
+        return response([
+            'code' => 500,
+            'messages' => $e->getMessage(),
+            'data' => []
+        ], 500);
     }
 }
